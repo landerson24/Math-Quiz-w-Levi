69d8bfb44d8e00fa7e90d38c8f53fa456b77b623
landerson24 <131026239+landerson24@users.noreply.github.com>
2023-04-25 01:53:33 +0000

    Final Project

--- Math Quiz w/MathQuiz.cpp
+++ Math Quiz w/MathQuiz.cpp
@@ -28,151 +28,144 @@ int HardDifficultyNumberGen() {
 	return hardNum;
 }
 
-void sortVectors(){
-    for (int i = 0; i < userScores.size() -1; i++){
-        int maxIndex = i;
-        for (int j = i + 1; j < userScores.size(); j++) {
-            if (userScores.at(j) > userScores.at(maxIndex)) {
-                maxIndex = j;
-            }
-        }
-        swap(userScores.at(maxIndex), userScores.at(i));
-        swap(userNames.at(maxIndex), userNames.at(i));
-    }
-}
-
-void highScores (){
-	cout << "The high scores are..." << endl;
-	
-	sortVectors();
-	
-	for (int i = 0; i < userNames.size(); i++){
-	    cout << i + 1 << ". " << userNames.at(i) << " with a score of " << userScores.at(i) << " out of 10!" << endl;
+void sortVectors() {
+	for (int i = 0; i < userScores.size() - 1; i++) {
+		int maxIndex = i;
+		for (int j = i + 1; j < userScores.size(); j++) {
+			if (userScores.at(j) > userScores.at(maxIndex)) {
+				maxIndex = j;
+			}
+		}
+		swap(userScores.at(maxIndex), userScores.at(i));
+		swap(userNames.at(maxIndex), userNames.at(i));
 	}
 }
 
-void getUserName(){
-    cout << "Please enter your name" << endl;
-    string userName;
-    cin >> userName;
-    cout << endl;
-    userNames.push_back(userName);
-    userScores.push_back(numCorrect);
-    numCorrect = 0;
+void highScores() {
+	cout << "The high scores are..." << endl;
+
+	sortVectors();
+
+	for (int i = 0; i < userNames.size(); i++) {
+		cout << i + 1 << ". " << userNames.at(i) << " with a score of " << userScores.at(i) << " out of 10!" << endl;
+	}
 }
 
-void displayNumCorrect(){
+void getUserName() {
+	cout << "Please enter your name" << endl;
+	string userName;
+	cin >> userName;
+	cout << endl;
+	userNames.push_back(userName);
+	userScores.push_back(numCorrect);
+	numCorrect = 0;
+}
+
+void displayNumCorrect() {
 	if (numCorrect > 0 && numCorrect < 10) {
-	    	cout << "You got " << numCorrect << " questions correct!";
-        }
-    	else if (numCorrect == 10) {
-    		cout << "You got them all correct!!";
-    	}
-    	else {
-	    	cout << "You got " << numCorrect << " questions correct :(";
-    	}
-    	cout << endl;
+		cout << "You got " << numCorrect << " questions correct!";
+	}
+	else if (numCorrect == 10) {
+		cout << "You got them all correct!!";
+	}
+	else {
+		cout << "You got " << numCorrect << " questions correct :(";
+	}
+	cout << endl;
 }
 
-void findUserDifficulty (DifficultyOptions& difficulty){
+void findUserDifficulty(DifficultyOptions& difficulty) {
 	cout << "Choose difficulty: Easy, Medium, or Hard" << endl;
-		string userChoice;
-	    cin >> userChoice;
+	string userChoice;
+	cin >> userChoice;
 
-        if (userChoice == "Easy" || userChoice == "easy") {
-		    difficulty = D_EASY;
-		    cout << "Difficulty: Easy" << endl;
-	    }
-	    else if (userChoice == "Medium" || userChoice == "medium") {
-		    difficulty = D_MEDIUM;
-		    cout << "Difficulty: Medium" << endl;
-	    }
-	    else if (userChoice == "Hard" || userChoice == "hard") {
-		    difficulty = D_HARD;
-		    cout << "Difficulty: Hard" << endl;
-	    }
-	    else {
-	    	cout << "Invalid difficulty" << endl;
-	    }
-	    cout << endl;
+	if (userChoice == "Easy" || userChoice == "easy") {
+		difficulty = D_EASY;
+		cout << "Difficulty: Easy" << endl;
+	}
+	else if (userChoice == "Medium" || userChoice == "medium") {
+		difficulty = D_MEDIUM;
+		cout << "Difficulty: Medium" << endl;
+	}
+	else if (userChoice == "Hard" || userChoice == "hard") {
+		difficulty = D_HARD;
+		cout << "Difficulty: Hard" << endl;
+	}
+	else {
+		cout << "Invalid difficulty" << endl;
+	}
+	cout << endl;
 }
 
-/*void unitTest(){
-    vector<string> userNamesTest = {"Billy Bob", "Logano", "Levi", "Jimy", "Buster"};
-    vector<int> userScoresTest = {8, 10, 6, 5, 9};
-    
-    vector<string> userNamesTestCorrect = {"Logano", "Buster", "Billy Bob", "Levi", "Jimy"};
-    vector<int> userScoresTestCorrect = {10, 9, 8, 6, 5};
-    
-    sortVectors();
-
-    assert(userNamesTest == userNamesTestCorrect && userScoresTest == userScoresTestCorrect);
-}*/
-
-bool takeAgain(){
-	string takeAgainString;
-	cout << "Would you like to take another Quiz? Type yes or no" << endl;
-    cin >> takeAgainString;
-    cout << endl;
-	if (takeAgainString == "no" || takeAgainString == "No" || takeAgainString == "NO"){
+bool takeAgain(string againString) {
+	cout << endl;
+	if (againString == "no" || againString == "No" || againString == "NO") {
 		return false;
 	}
 	return true;
 }
 
-void correctAnswerForSub(int answer, int randomNum, int randomNum2){
-	if (answer == randomNum - randomNum2){
-		    cout << "Correct" << endl;
-	    	numCorrect += 1;
-	    }
+void unitTest(){
+	string unitString;
+	unitString = "yes";
+	assert(takeAgain(unitString) == true);
+	unitString = "no";
+	assert(takeAgain(unitString) == false);
+}
+
+void correctAnswerForSub(int answer, int randomNum, int randomNum2) {
+	if (answer == randomNum - randomNum2) {
+		cout << "Correct" << endl;
+		numCorrect += 1;
+	}
 	else {
 		cout << "Incorrect, try again" << endl;
 		cout << randomNum << " - " << randomNum2 << " = ?" << endl;
-		cin  >> answer;
-		if (answer == randomNum - randomNum2){
-		    cout << "Correct" << endl;
-		    numCorrect += 1;
+		cin >> answer;
+		if (answer == randomNum - randomNum2) {
+			cout << "Correct" << endl;
+			numCorrect += 1;
 		}
-		else{
-    		cout << "Incorrect, the correct answer was " << randomNum + randomNum2 << endl;
-    		cout << "Next question" << endl;
+		else {
+			cout << "Incorrect, the correct answer was " << randomNum + randomNum2 << endl;
+			cout << "Next question" << endl;
 		}
 	}
 }
 
-void correctAnswerForAdd(int answer, int randomNum, int randomNum2){
-	if (answer == randomNum + randomNum2){
-		    cout << "Correct" << endl;
-	    	numCorrect += 1;
-	    }
+void correctAnswerForAdd(int answer, int randomNum, int randomNum2) {
+	if (answer == randomNum + randomNum2) {
+		cout << "Correct" << endl;
+		numCorrect += 1;
+	}
 	else {
 		cout << "Incorrect, try again" << endl;
 		cout << randomNum << " + " << randomNum2 << " = ?" << endl;
-		cin  >> answer;
-		if (answer == randomNum + randomNum2){
-		    cout << "Correct" << endl;
-		    numCorrect += 1;
+		cin >> answer;
+		if (answer == randomNum + randomNum2) {
+			cout << "Correct" << endl;
+			numCorrect += 1;
 		}
-		else{
-    		cout << "Incorrect, the correct answer was " << randomNum + randomNum2 << endl;
-    		cout << "Next question" << endl;
+		else {
+			cout << "Incorrect, the correct answer was " << randomNum + randomNum2 << endl;
+			cout << "Next question" << endl;
 		}
 	}
 }
 
-void subDifficultyQuestions(DifficultyOptions difficulty){
+void subDifficultyQuestions(DifficultyOptions difficulty) {
 	int randomNum;
 	int randomNum2;
 
-	if (difficulty == D_EASY){
+	if (difficulty == D_EASY) {
 		randomNum = EasyDifficultyNumberGen();
 		randomNum2 = EasyDifficultyNumberGen();
 	}
-	if (difficulty == D_MEDIUM){
+	if (difficulty == D_MEDIUM) {
 		randomNum = MediumDifficultyNumberGen();
 		randomNum2 = MediumDifficultyNumberGen();
 	}
-	if (difficulty == D_HARD){
+	if (difficulty == D_HARD) {
 		randomNum = EasyDifficultyNumberGen();
 		randomNum2 = EasyDifficultyNumberGen();
 	}
@@ -185,37 +178,41 @@ void subDifficultyQuestions(DifficultyOptions difficulty){
 	correctAnswerForSub(answer, randomNum, randomNum2);
 }
 
-void subtractionQuiz(){
+void subtractionQuiz() {
 	DifficultyOptions difficulty = D_EASY;
+	string takeAgainString;
 
-	do{
+	do {
 		findUserDifficulty(difficulty);
 
-	    for (int i = 0; i < 10; i++){
-	    	subDifficultyQuestions(difficulty);
-    	}
-    	cout << endl;
-    	displayNumCorrect();
-    	
-		if (numCorrect > 0){
-    		getUserName();
+		for (int i = 0; i < 10; i++) {
+			subDifficultyQuestions(difficulty);
 		}
-	} while (takeAgain());
+		cout << endl;
+		displayNumCorrect();
+
+		if (numCorrect > 0) {
+			getUserName();
+		}
+		
+		cout << "Would you like to take another Quiz? Type yes or no" << endl;
+		cin >> takeAgainString;
+	} while (takeAgain(takeAgainString));
 }
 
-void additionDifficultyQuestions(DifficultyOptions difficulty){
+void additionDifficultyQuestions(DifficultyOptions difficulty) {
 	int randomNum;
 	int randomNum2;
 
-	if (difficulty == D_EASY){
+	if (difficulty == D_EASY) {
 		randomNum = EasyDifficultyNumberGen();
 		randomNum2 = EasyDifficultyNumberGen();
 	}
-	if (difficulty == D_MEDIUM){
+	if (difficulty == D_MEDIUM) {
 		randomNum = MediumDifficultyNumberGen();
 		randomNum2 = MediumDifficultyNumberGen();
 	}
-	if (difficulty == D_HARD){
+	if (difficulty == D_HARD) {
 		randomNum = EasyDifficultyNumberGen();
 		randomNum2 = EasyDifficultyNumberGen();
 	}
@@ -228,57 +225,61 @@ void additionDifficultyQuestions(DifficultyOptions difficulty){
 	correctAnswerForAdd(answer, randomNum, randomNum2);
 }
 
-void additionQuiz(){
+void additionQuiz() {
 	DifficultyOptions difficulty = D_EASY;
+	string takeAgainString;
 
-	do{
+	do {
 		findUserDifficulty(difficulty);
 
-	    for (int i = 0; i < 10; i++){
-	    	additionDifficultyQuestions(difficulty);
-    	}
-    	cout << endl;
-    	displayNumCorrect();
-    	
-		if (numCorrect > 0){
-    		getUserName();
+		for (int i = 0; i < 10; i++) {
+			additionDifficultyQuestions(difficulty);
 		}
-	} while (takeAgain());
+		cout << endl;
+		displayNumCorrect();
+
+		if (numCorrect > 0) {
+			getUserName();
+		}
+
+		cout << "Would you like to take another Quiz? Type yes or no" << endl;
+		cin >> takeAgainString;
+	} while (takeAgain(takeAgainString));
 }
 
-void correctAnswerForMult(int answer, int randomNum, int randomNum2){
-	if (answer == randomNum * randomNum2){
-		    cout << "Correct" << endl;
-	    	numCorrect += 1;
-	    }
+void correctAnswerForMult(int answer, int randomNum, int randomNum2) {
+	if (answer == randomNum * randomNum2) {
+		cout << "Correct" << endl;
+		numCorrect += 1;
+	}
 	else {
 		cout << "Incorrect, try again" << endl;
 		cout << randomNum << " * " << randomNum2 << " = ?" << endl;
-		cin  >> answer;
-		if (answer == randomNum * randomNum2){
-		    cout << "Correct" << endl;
-		    numCorrect += 1;
+		cin >> answer;
+		if (answer == randomNum * randomNum2) {
+			cout << "Correct" << endl;
+			numCorrect += 1;
 		}
-		else{
-    		cout << "Incorrect, the correct answer was " << randomNum + randomNum2 << endl;
-    		cout << "Next question" << endl;
+		else {
+			cout << "Incorrect, the correct answer was " << randomNum + randomNum2 << endl;
+			cout << "Next question" << endl;
 		}
 	}
 }
 
-void multDifficultyQuestions(DifficultyOptions difficulty){
+void multDifficultyQuestions(DifficultyOptions difficulty) {
 	int randomNum;
 	int randomNum2;
 
-	if (difficulty == D_EASY){
+	if (difficulty == D_EASY) {
 		randomNum = EasyDifficultyNumberGen();
 		randomNum2 = EasyDifficultyNumberGen();
 	}
-	if (difficulty == D_MEDIUM){
+	if (difficulty == D_MEDIUM) {
 		randomNum = MediumDifficultyNumberGen();
 		randomNum2 = MediumDifficultyNumberGen();
 	}
-	if (difficulty == D_HARD){
+	if (difficulty == D_HARD) {
 		randomNum = EasyDifficultyNumberGen();
 		randomNum2 = EasyDifficultyNumberGen();
 	}
@@ -291,64 +292,74 @@ void multDifficultyQuestions(DifficultyOptions difficulty){
 	correctAnswerForMult(answer, randomNum, randomNum2);
 }
 
-void multQuiz(){
+void multQuiz() {
 	DifficultyOptions difficulty = D_EASY;
+	string takeAgainString;
 
-	do{
+	do {
 		findUserDifficulty(difficulty);
 
-	    for (int i = 0; i < 10; i++){
-	    	multDifficultyQuestions(difficulty);
-    	}
-    	cout << endl;
-    	displayNumCorrect();
-    	
-		if (numCorrect > 0){
-    		getUserName();
+		for (int i = 0; i < 10; i++) {
+			multDifficultyQuestions(difficulty);
 		}
-	} while (takeAgain());
+		cout << endl;
+		displayNumCorrect();
+
+		if (numCorrect > 0) {
+			getUserName();
+		}
+		
+		cout << "Would you like to take another Quiz? Type yes or no" << endl;
+		cin >> takeAgainString;
+	} while (takeAgain(takeAgainString));
 }
 
-void correctAnswerForDiv(int answer, int randomNum, int randomNum2){
-	if (answer == randomNum / randomNum2){
-		    cout << "Correct" << endl;
-	    	numCorrect += 1;
-	    }
+void correctAnswerForDiv(int answer, int randomNum, int randomNum2) {
+	if (answer == randomNum / randomNum2) {
+		cout << "Correct" << endl;
+		numCorrect += 1;
+	}
 	else {
 		cout << "Incorrect, try again" << endl;
 		cout << randomNum << " / " << randomNum2 << " = ?" << endl;
-		cin  >> answer;
-		if (answer == randomNum / randomNum2){
-		    cout << "Correct" << endl;
-		    numCorrect += 1;
+		cin >> answer;
+		if (answer == randomNum / randomNum2) {
+			cout << "Correct" << endl;
+			numCorrect += 1;
 		}
-		else{
-    		cout << "Incorrect, the correct answer was " << randomNum + randomNum2 << endl;
-    		cout << "Next question" << endl;
+		else {
+			cout << "Incorrect, the correct answer was " << randomNum + randomNum2 << endl;
+			cout << "Next question" << endl;
 		}
 	}
 }
 
-void divDifficultyQuestions(DifficultyOptions difficulty){
+void divDifficultyQuestions(DifficultyOptions difficulty) {
 	int randomNum;
 	int randomNum2;
 
-	if (difficulty == D_EASY){
+	if (difficulty == D_EASY) {
 		randomNum = EasyDifficultyNumberGen();
 		randomNum2 = EasyDifficultyNumberGen();
-		while (randomNum2 == 0){
+		while (randomNum2 == 0 || randomNum <= randomNum2 || !(randomNum % randomNum2 == 0) ) {
+			randomNum = EasyDifficultyNumberGen();
 			randomNum2 = EasyDifficultyNumberGen();
 		}
 	}
-	if (difficulty == D_MEDIUM){
+	if (difficulty == D_MEDIUM) {
 		randomNum = MediumDifficultyNumberGen();
 		randomNum2 = MediumDifficultyNumberGen();
+		while (randomNum2 == 0 || randomNum <= randomNum2 || !(randomNum % randomNum2 == 0) ) {
+			randomNum = MediumDifficultyNumberGen();
+			randomNum2 = MediumDifficultyNumberGen();
+		}
 	}
-	if (difficulty == D_HARD){
-		randomNum = EasyDifficultyNumberGen();
-		randomNum2 = EasyDifficultyNumberGen();
-		while (randomNum2 == 0){
-			randomNum2 = EasyDifficultyNumberGen();
+	if (difficulty == D_HARD) {
+		randomNum = HardDifficultyNumberGen();
+		randomNum2 = HardDifficultyNumberGen();
+		while (randomNum2 == 0 || randomNum <= randomNum2 || !(randomNum % randomNum2 == 0) ) {
+			randomNum = HardDifficultyNumberGen();
+			randomNum2 = HardDifficultyNumberGen();
 		}
 	}
 
@@ -360,46 +371,50 @@ void divDifficultyQuestions(DifficultyOptions difficulty){
 	correctAnswerForDiv(answer, randomNum, randomNum2);
 }
 
-void divisionQuiz(){
+void divisionQuiz() {
 	DifficultyOptions difficulty = D_EASY;
+	string takeAgainString;
 	cout << "All answers will be integers: Round down if answer contains a decimal." << endl;
-	do{
+	do {
 		findUserDifficulty(difficulty);
 
-	    for (int i = 0; i < 10; i++){
-	    	divDifficultyQuestions(difficulty);
-    	}
-    	cout << endl;
-    	displayNumCorrect();
-    	
-		if (numCorrect > 0){
-    		getUserName();
+		for (int i = 0; i < 10; i++) {
+			divDifficultyQuestions(difficulty);
 		}
-	} while (takeAgain());
+		cout << endl;
+		displayNumCorrect();
+
+		if (numCorrect > 0) {
+			getUserName();
+		}
+		
+		cout << "Would you like to take another Quiz? Type yes or no" << endl;
+		cin >> takeAgainString;
+	} while (takeAgain(takeAgainString));
 }
 
 int main() {
-    //unitTest();
+	unitTest();
 	srand(time(0));
 	string quizType;
-	
+
 	cout << "Choose a type of quiz: Addition, Subtraction, Multiplication, or Division" << endl;
 	cin >> quizType;
 	cout << "Type: " << quizType << endl;
-	if (quizType == "addition" || quizType == "Addition"){
+	if (quizType == "addition" || quizType == "Addition") {
 		additionQuiz();
 	}
-	if (quizType == "subtraction" || quizType == "Subtraction"){
+	if (quizType == "subtraction" || quizType == "Subtraction") {
 		subtractionQuiz();
 	}
-	if (quizType == "multiplication" || quizType == "Multiplication"){
+	if (quizType == "multiplication" || quizType == "Multiplication") {
 		multQuiz();
 	}
-	if (quizType == "division" || quizType == "Division"){
+	if (quizType == "division" || quizType == "Division") {
 		divisionQuiz();
 	}
 
 	highScores();
-	
+
 	return 0;
 }
